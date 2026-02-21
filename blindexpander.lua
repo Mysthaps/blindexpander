5c511047c68bb57b5038e8341d911b0f8f90ffe7
Alvin Dong <alvindong9913@gmail.com>
2026-02-21 00:34:16 -0600

    verbump

--- blindexpander.lua
+++ blindexpander.lua
@@ -9,7 +9,7 @@
 --- If passive description is too long, changing how it is formatted instead of changing UIBox width is preferred
 
 to_big = to_big or function(x) return x end
-local BLINDEXPANDER_VERSION = 102020
+local BLINDEXPANDER_VERSION = 102030
 
 local function startup()
     if blindexpander.started_up then return end

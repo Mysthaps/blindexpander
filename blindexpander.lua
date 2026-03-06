0bfbf949063df0abc0cf467b2f7f00bc442a5ad4
Alvin Dong <alvindong9913@gmail.com>
2026-03-06 10:39:19 -0600

    verbump

--- blindexpander.lua
+++ blindexpander.lua
@@ -11,7 +11,7 @@
 to_big = to_big or function(x)
 	return x
 end
-local BLINDEXPANDER_VERSION = 102070
+local BLINDEXPANDER_VERSION = 102080
 
 local function startup()
 	if blindexpander.started_up then

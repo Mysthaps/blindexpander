776b1c22eb6d96860aa2f031d06810b3c57dd2ea
ThunderEdge <alvindong9913@gmail.com>
2026-06-06 22:31:52 -0500

    verbump

--- blindexpander.lua
+++ blindexpander.lua
@@ -11,7 +11,7 @@
 to_big = to_big or function(x)
 	return x
 end
-local BLINDEXPANDER_VERSION = 102092
+local BLINDEXPANDER_VERSION = 102093
 
 local function startup()
 	if blindexpander.started_up then
